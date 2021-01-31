55fe9be4b5c7463c45eafa35dee847945467165e
Chiranjit Dey <chiranjitdey2000@gmail.com>
2021-01-31 18:37:57 +0530

    dummy data stored

---
+++ index.js
@@ -0,0 +1,45 @@
+const quiz_data = [
+    {
+        question: 'What is my main character in Valo?',
+        a: 'Jett',
+        b: 'Killjoy',
+        c: 'Sage',
+        d: 'Yoru',
+        correct: 'a'
+    }, {
+        question: 'Most popular programming language?',
+        a: 'Java',
+        b: 'Javascript',
+        c: 'Python',
+        d: 'C',
+        correct: 'b'
+    },{
+        question: 'Who is the Prime Minister of India?',
+        a: 'Nirav Modi',
+        b: 'Lalit Modi',
+        c: 'Narendra Modi',
+        d: 'Mamata Banerjee',
+        correct: 'c'
+    },{
+        question: 'Which team won the last UCL?',
+        a: 'Real Madrid FC',
+        b: 'AC Milan',
+        c: 'Barcelona FC',
+        d: 'Bayern Munich FC',
+        correct: 'd'
+    }, {
+        question: 'Origin of Covid-19?',
+        a: 'India',
+        b:'China',
+        c: 'South Korea',
+        d: 'Japan',
+        correct: 'b'
+    },{
+        question: 'Who developed VSCODE stories?',
+        a: 'Mosh Hamedami',
+        b: 'Shaun Michael',
+        c: 'Ben Awad',
+        d: 'Clement Something',
+        correct: 'c'
+    }
+];
